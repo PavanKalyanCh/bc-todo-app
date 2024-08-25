d5c8084460d4c9ea21cc3867493ac6ad87c4a536
pavankalyan <pavankalyan.ch004@gmail.com>
2024-08-25 22:29:47 +0530

    pushing files

--- truffle-config.js
+++ truffle-config.js
@@ -2,14 +2,17 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
-    }
+      port: 8545,
+      network_id: "5777" // Match Ganache's network id
+    },
   },
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
+  compilers: {
+    solc: {
+      version: "0.8.0", // Specify the Solidity compiler version
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
     }
   }
-}
+};
