eb32a91c752fd8abb1c931af4c10f984f780baa2
pavankalyan <pavankalyan.ch004@gmail.com>
2024-08-25 22:53:42 +0530

    pushing files

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+
 module.exports = {
   networks: {
     development: {
@@ -5,10 +8,21 @@ module.exports = {
       port: 8545,
       network_id: "5777" // Match Ganache's network id
     },
+    ropsten: {
+      provider: () => new HDWalletProvider(
+        process.env.DEPLOYER_PRIVATE_KEY, 
+        process.env.ALCHEMY_API_URL
+      ),
+      network_id: 3, // Ropsten's id
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    }
   },
   compilers: {
     solc: {
-      version: "0.7.6", // Specify the Solidity compiler version
+      version: "0.8.0", // Adjust as needed to match your contracts
       optimizer: {
         enabled: true,
         runs: 200
